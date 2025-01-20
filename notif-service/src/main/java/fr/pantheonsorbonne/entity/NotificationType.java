17bbfaaa4f95cd9d29df673f92882705fedb3e0a
alexis <angabala@outlook.com>
2025-01-20 11:34:52 +0100

    g tout suppr

--- notif-service/src/main/java/fr/pantheonsorbonne/entity/NotificationType.java
+++
@@ -1,7 +0,0 @@
-package fr.pantheonsorbonne.entity;
-
-public enum NotificationType {
-    RESERVATION_CONFIRMED, // Pour confirmer une réservation
-    RESERVATION_CANCELLED, // Pour une annulation de réservation
-    TRAJET_REMINDER        // Pour un rappel de trajet
-}
