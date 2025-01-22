d3da1dd6fca0021cd8ae9d1928c4ff7d4d3701dc
Sarahgrj <guerroujsarahh@gmail.com>
2025-01-22 09:26:29 +0100

    ajout des fichiers non suivi avant pull

--- recherche-service/src/main/java/fr/pantheonsorbonne/gateway/ReservationGateway.java
+++ recherche-service/src/main/java/fr/pantheonsorbonne/gateway/ReservationGateway.java
@@ -1,4 +1,27 @@
 package fr.pantheonsorbonne.gateway;
 
+import org.apache.camel.ProducerTemplate;
+
+import fr.pantheonsorbonne.dto.ResultatDTO;
+import fr.pantheonsorbonne.exception.ReservationCommunicationException;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+
 public class ReservationGateway {
+
+    @Inject
+    private ProducerTemplate producerTemplate;
+
+    // Envoie du resultat à la reservation
+
+    public void envoyerResultatReservation(ResultatDTO resultatDTO) {
+        try {
+            producerTemplate.sendBody("direct:sendResultatReservation", resultatDTO);
+        } catch (Exception e) {
+            throw new ReservationCommunicationException("Erreur lors de l'envoi des résultats au microservice réservation.", e);
+        }
+    }
 }
+
